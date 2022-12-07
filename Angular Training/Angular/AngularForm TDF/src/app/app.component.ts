c23a806fcadcd41e5c1e7471ac9ae4c785a611f9
Prabhudev Shivayogi Betsurmath <prabhudev.s@intimetec.com>
2022-12-07 14:23:03 +0530

    TDF

---
+++ Angular Training/Angular/AngularForm TDF/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { User } from './user';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  topics = ['Angular', 'React', 'Vue'];
+  user = new User("","bcs","csdcds","Angular","dscds",true)
+
+}
